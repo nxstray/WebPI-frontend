66e97b02cfe1adc198f66757a43e227180e6b9ba
nxstray <afwanapriansyah@gmail.com>
2025-07-02 04:29:38 +0700

    adding new path: done

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { BivariateComponent } from './pages/bivariate/bivariate.component';
+import { AnovaComponent } from './pages/anova/anova.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent:() => import ('./pages/home/home.component').then(m =>HomeComponent)
+  },
+  {
+    path: 'bivariate',
+    loadComponent:() => import ('./pages/bivariate/bivariate.component').then(m =>BivariateComponent)
+  },
+  {
+    path: 'anova',
+    loadComponent:() => import ('./pages/anova/anova.component').then(m =>AnovaComponent)
+  }
+];
