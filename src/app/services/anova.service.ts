f2f50f2f3542896ce7e0ecfaa2203a8756d3279f
nxstray <afwanapriansyah@gmail.com>
2025-07-04 05:42:07 +0700

    setting up API for BE to FE: done

---
+++ src/app/services/anova.service.ts
@@ -0,0 +1,49 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+export interface AnovaDTO {
+  namaKasus: string;
+  namaVariabelDependen: string;
+  ho: string;
+  ha: string;
+  alpha: number;
+  inputMethod: string;
+  namaGrup: string[];
+  nilaiGrup: number[][];
+}
+
+export interface GrupDTO {
+  grup: string;
+  nilai: number;
+}
+
+export interface AnovaResponseDTO {
+  idAnova: number;
+  namaKasus: string;
+  namaVarY: string;
+  ho: string;
+  ha: string;
+  alpha: number;
+  inputMethod: string;
+  grups: GrupDTO[];
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AnovaService {
+  private apiUrl = 'http://localhost:6060/api/anova';
+
+  currentResult: AnovaResponseDTO | null = null;
+
+  constructor(private http: HttpClient) {}
+
+  simpanAnova(data: AnovaDTO): Observable<any> {
+    return this.http.post(this.apiUrl, data);
+  }
+
+  getAnovaById(id: number): Observable<AnovaResponseDTO> {
+    return this.http.get<AnovaResponseDTO>(`${this.apiUrl}/${id}`);
+  }
+}
