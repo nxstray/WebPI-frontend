e91f2a729ba59742eb25aad6181548384aa00c0a
nxstray <afwanapriansyah@gmail.com>
2025-07-14 01:58:42 +0700

    updating without key

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: process.env['NG_ENV_API_URL'] || 'https://webpi-backend-production.up.railway.app/api'
+  apiUrl: 'https://webpi-backend-production.up.railway.app/api'
 };
